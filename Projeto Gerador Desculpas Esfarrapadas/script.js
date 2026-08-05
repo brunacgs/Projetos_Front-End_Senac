fa789e412c0c2f66134ea79ebcb1fa30928acf36
Bruna Cardoso Gomes <brunacardosogomes@hotmail.com>
2026-08-04 21:46:07 -0300

    Refactor script.js to add new functions

--- Projeto Gerador Desculpas Esfarrapadas/script.js
+++ Projeto Gerador Desculpas Esfarrapadas/script.js
@@ -1,11 +1,24 @@
-let desculpa1 = document.getElementById("desculpaaleatoria")
+let desculpaaleatoria = document.getElementById("desculpaaleatoria");
 
 function alerta()
 {
     alert("Versão Beta 0.1. Aviso: O uso excessivo pode causar demissão!");
 }
 
-function mudar()
+function gerardica()
 {
-    desculpa1.textContent = "Dica: Culpe sempre o transporte público. Ninguém discute com o metrô."
+    desculpaaleatoria.textContent = "Dica: Culpe sempre o transporte público. Ninguém discute com o metrô.";
+}
+
+function personalizardesculpa()
+{
+    let nomechefe = document.getElementById("nomechefe").value;
+    alert("Querido(a) " + nomechefe + ", o pneu do ônibus furou na Via Expressa! Tive que empurrar!");
+}
+
+function calcularstress()
+{
+    let minutos = Number(document.getElementById("minutos").value);
+    let notastress = Number(document.getElementById("notastress").value);
+    alert("Seu atraso equivale a " + minutos*notastress + " unidades de estresse acumulado do seu superior.");
 }
