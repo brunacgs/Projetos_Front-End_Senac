eb23eddf73ba8539d889944876621fff747052e7
Bruna Cardoso Gomes <brunacardosogomes@hotmail.com>
2026-08-14 21:49:14 -0300

    Add promotional activation function
    Implement promotional display for client with pricing details.

---
+++ Projeto Site Tecnologia_games/script.js
@@ -0,0 +1,37 @@
+function ativarpromo()
+{
+    let cliente = document.getElementById("cliente").value;
+
+    document.getElementById("saudacoes").textContent =
+        "Saudações " + cliente + "!!! Veja nossos destaques da semana e promções especiais para você:";
+
+    document.getElementById("preco1").textContent = 
+        "De R$120,00";
+    preco1.style.textDecoration = "line-through";
+    preco1.style.color = "#7C3AED";
+    preco1.style.fontSize = "18px";    
+    document.getElementById("preco11").textContent = 
+        "por R$105,00";
+    preco11.style.color = "#22D3EE";
+    preco11.style.fontSize = "22px";
+    
+        document.getElementById("preco2").textContent = 
+        "De R$150,00";
+    preco2.style.textDecoration = "line-through";
+    preco2.style.color = "#7C3AED";
+    preco2.style.fontSize = "18px";
+    document.getElementById("preco21").textContent = 
+        "por R$135,00";
+    preco21.style.color = "#22D3EE";
+    preco21.style.fontSize = "22px";
+  
+        document.getElementById("preco3").textContent = 
+        "De R$220,00";
+    preco3.style.textDecoration = "line-through";
+    preco3.style.color = "#7C3AED";
+    preco3.style.fontSize = "18px";
+    document.getElementById("preco31").textContent = 
+        "por R$205,00";
+    preco31.style.color = "#22D3EE";
+    preco31.style.fontSize = "22px";
+}
