8a8fcc4db11773a3257bdf694ba18fae95bf7483
Bruna Cardoso Gomes <brunacardosogomes@hotmail.com>
2026-08-17 21:40:45 -0300

    Revise promo function with new prices and discount
    Updated promotional prices and added discount message.

--- Projeto Site Tecnologia_games/script.js
+++ Projeto Site Tecnologia_games/script.js
@@ -1,9 +1,14 @@
+function vercatalogo()
+{
+
+}
+
 function ativarpromo()
 {
     let cliente = document.getElementById("cliente").value;
 
     document.getElementById("saudacoes").textContent =
-        "Saudações " + cliente + "!!! Veja nossos destaques da semana e promções especiais para você:";
+        "Saudações " + cliente + "!!! Veja nossos destaques da semana e ganhe *10% de desconto na compra de 3 produtos*!";
 
     document.getElementById("preco1").textContent = 
         "De R$120,00";
@@ -11,7 +16,7 @@ function ativarpromo()
     preco1.style.color = "#7C3AED";
     preco1.style.fontSize = "18px";    
     document.getElementById("preco11").textContent = 
-        "por R$105,00";
+        "por R$108,00";
     preco11.style.color = "#22D3EE";
     preco11.style.fontSize = "22px";
     
@@ -31,7 +36,7 @@ function ativarpromo()
     preco3.style.color = "#7C3AED";
     preco3.style.fontSize = "18px";
     document.getElementById("preco31").textContent = 
-        "por R$205,00";
+        "por R$198,00";
     preco31.style.color = "#22D3EE";
     preco31.style.fontSize = "22px";
 }
