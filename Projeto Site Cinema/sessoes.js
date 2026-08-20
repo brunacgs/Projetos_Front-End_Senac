e19da81361272ac6d7c46c77e7c3f864360ac7bf
Bruna Cardoso Gomes <brunacardosogomes@hotmail.com>
2026-08-19 21:47:30 -0300

    Add files via upload

---
+++ Projeto Site Cinema/sessoes.js
@@ -0,0 +1,92 @@
+const sessoes = [
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "O Lobo de Wall Street: Versão Planilha",
+        gênero: "Drama",
+        classificação: "18 anos",
+        sinopse: "Ganhar dinheiro é fácil! Difícil é explicar no imposto."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Velozes e Furiosos no Uber",
+        gênero: "Ação",
+        classificação: "16",
+        sinopse: "Corridas ilegais agora disputadas por corrida dinâmica e avaliação 5 estrelas."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Jurassic Park: O Parque dos Gatinhos",
+        gênero: "Aventura",
+        classificação: "L",
+        sinopse: "Milhares de gatinhos clonados prontos para dominar o mundo."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Vingadores: Ultimato - A Fila do Banco",
+        gênero: "Ação",
+        classificação: "16",
+        sinopse: "Os heróis mais poderosos da Terra passam três horas tentando resolver um problema burocrático no sistema intergaláctico."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "O Poderoso Chefão: A Reunião de Condomínio",
+        gênero: "Crime",
+        classificação: "18",
+        sinopse: "Don Corleone resolve conflitos de vizinhança usando propostas irrecusáveis sobre o barulho do cachorro do 4º andar."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Crepúsculo: A Insônia Eterna",
+        gênero: "Romance",
+        classificação: "16",
+        sinopse: "Uma adolescente se apaixona por um vampiro brilhante que passa 100 anos cursando o ensino médio por pura falta de plano de carreira."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Interestelar: Perdidos no Trânsito",
+        gênero: "Ficção Científica / Drama",
+        classificação: "16",
+        sinopse: "Astronautas viajam através de buracos de minhoca para achar um novo planeta, mas acabam presos em um engarrafamento cósmico de três décadas."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "Matrix: Modo Home Office",
+        gênero: "Ficção Científica",
+        classificação: "18",
+        sinopse: "Trabalhar ou maratonar séries? A verdade dói menos quando você está de pijama"
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "The Office",
+        gênero: "Comédia/ Drama",
+        classificação: "14",
+        sinopse: "Trabalhar é sóbreviver a reuniões que poderiam ter sido emails."
+    },
+    {
+        data: "", 
+        horário: "",
+        sala: "",
+        filme: "",
+        gênero: "",
+        classificação: "",
+        sinopse: ""
+    }
+]
